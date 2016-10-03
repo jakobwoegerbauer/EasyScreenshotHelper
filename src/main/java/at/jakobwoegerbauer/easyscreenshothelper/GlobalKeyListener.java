96c9433d1a0ccb002f918e714481e10632741ba1
Jakob Wögerbauer <jakob.woegerbauer@gmx.at>
2016-10-03 13:42:05 +0200

    add missing files

---
+++ src/main/java/at/jakobwoegerbauer/easyscreenshothelper/GlobalKeyListener.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.jakobwoegerbauer.easyscreenshothelper;
+
+import java.util.Observable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.input.KeyEvent;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+/**
+ *
+ * @author jakob
+ */
+public class GlobalKeyListener extends Observable implements NativeKeyListener {
+	private final int keyCode;
+	private boolean controlDown;
+	
+	private static final int CONTROL_KEY = 29; //0xA4; //29;
+	
+	public GlobalKeyListener(Configuration config){
+		this.keyCode = config.getKeyCode();
+		this.controlDown = false; 
+	}
+	
+	@Override
+	public void nativeKeyPressed(NativeKeyEvent nke) {
+		if(nke.getKeyCode() == CONTROL_KEY){
+			controlDown = true;
+			Logger.getLogger("GlobalKeyListener").log(Level.SEVERE, "control key down");
+		}
+	}
+
+	@Override
+	public void nativeKeyReleased(NativeKeyEvent nke) {
+		if(nke.getKeyCode() == CONTROL_KEY){
+			controlDown = false;
+			Logger.getLogger("GlobalKeyListener").log(Level.SEVERE, "control key up");
+			return;
+		}
+		if(nke.getKeyCode()== keyCode && controlDown){
+			Logger.getLogger("GlobalKeyListener").log(Level.SEVERE, "command detected");
+			setChanged();
+			notifyObservers();
+		}
+	}
+
+	@Override
+	public void nativeKeyTyped(NativeKeyEvent nke) {
+		
+	}
+	
+}
